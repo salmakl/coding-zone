17eee741a563cfc2849dfdcfa43f83396be48a35
salmakl <salma.kalkhi@gmail.com>
2022-01-07 11:57:13 +0100

    Dao

--- src/main/java/com/codingzone/models/Questins.java
+++ src/main/java/com/codingzone/models/Questins.java
@@ -1,4 +1,12 @@
 package com.codingzone.models;
 
+import java.sql.Timestamp;
+
 public class Questins {
+    private int id;
+    private String question;
+    private Timestamp limitTime;
+    private int id_answer;
+    private int id_category;
+    
 }
